6e31ead539bc255484d9459e120efee74eaf917c
nicolascalvo73 <nicolascalvo73@gmail.com>
2023-07-10 12:56:48 -0300

    character bug

--- server/src/controllers/getCharacterById.js
+++ server/src/controllers/getCharacterById.js
@@ -4,7 +4,8 @@ const URL = 'https://rickandmortyapi.com/api/character'
 const getCharById = async (req, res) => {
 	const { id } = req.params
 	try {
-		const { data } = await axios(`${URL}/${id}`)
+		const response = await axios(`${URL}/${id}`)
+		const { data } = response
 		let character = { ...character, origin: data.origin.name, location: data.location.name }
 		return character.name ? res.status(200).json(character) : res.status(404).send('Not found')
 	} catch (error) {
